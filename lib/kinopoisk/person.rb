7b023f6623a31e7821b36182a0218c3abc0243dc
Ravwar <rav_war@mail.ru>
2013-03-31 22:08:01 +0400

    Add more person accessor methods

--- lib/kinopoisk/person.rb
+++ lib/kinopoisk/person.rb
@@ -1,3 +1,4 @@
+#coding: UTF-8
 module Kinopoisk
   class Person
     attr_accessor :id, :url, :name
@@ -20,10 +21,54 @@ module Kinopoisk
       doc.search("//tr[./td/h1[@class='moviename-big']]/following-sibling::tr//span").text
     end
 
+    def partner
+      doc.search("//td[@class='type'][contains(text(),'супруг')]").first.next.text
+    end
+
+    def birthdate
+      Date.strptime doc.search("td.birth").first.attr 'birthdate'
+    end
+
+    def birthplace
+      search_by_text('место рождения').split(', ').first
+    end
+
+    def genres
+      search_by_text('жанры').split(', ')
+    end
+
+    def career
+      search_by_text('карьера').split(', ')
+    end
+
+    def total_movies
+      search_by_text('всего фильмов').to_i
+    end
+
+    def best_movies
+      doc.search('td.actor_list a').map(&:text)
+    end
+
+    def first_movie
+      search_by_text 'первый фильм'
+    end
+
+    def last_movie
+      search_by_text 'последний фильм'
+    end
+
+    def height
+      search_by_text 'рост'
+    end
+
     private
 
     def doc
       @doc ||= Kinopoisk.parse url
+    end
+
+    def search_by_text name
+      doc.search("//td[@class='type'][text()='#{name}']").first.next.text
     end
   end
 end
--- spec/kinopoisk/person_spec.rb
+++ spec/kinopoisk/person_spec.rb
@@ -8,7 +8,17 @@ describe Kinopoisk::Person, vcr: { cassette_name: 'person' } do
     person.url.should eq('http://www.kinopoisk.ru/name/13180/')
   end
 
+  it { person.best_movies.should include('Карты, деньги, два ствола', 'Братья по оружию') }
   it { person.poster.should eq('http://st.kinopoisk.ru/images/actor/13180.jpg') }
+  it { person.career.should eq(['Актер', 'Режиссер', 'Сценарист', 'Продюсер']) }
+  it { person.genres.should eq(['драма', 'комедия', 'криминал']) }
+  it { person.partner.should eq('Далия Ибельхауптайте') }
   it { person.name_en.should eq('Dexter Fletcher') }
   it { person.name.should eq('Декстер Флетчер') }
+  it { person.birthplace.should eq('Лондон') }
+  it { person.first_movie.should eq('1976') }
+  it { person.last_movie.should eq('2014') }
+  it { person.birthdate.should be_a(Date) }
+  it { person.total_movies.should eq(87) }
+  it { person.height.should eq('1.68 м') }
 end
