272596c6ab03db0547273170c3bb1fe80e15309e
Valeriy Utyaganov <mac-1@Mac-mini-mac-1.local>
2014-09-30 18:43:05 +0400

    With balancer

--- lib/kinopoisk_parser.rb
+++ lib/kinopoisk_parser.rb
@@ -12,7 +12,10 @@ module Kinopoisk
 
   # Headers are needed to mimic proper request so kinopoisk won't block it
   def self.fetch(url)
-    HTTPClient.new.get url, nil, { 'User-Agent'=>'a', 'Accept-Encoding'=>'a' }
+    config = YAML.load_file('./kinopoisk_parser.yml')
+    server_id = config['id'].to_i
+    config['id'] = (server_id + 1) % 5
+    HTTPClient.new.get "http://kinopoisk-parser-#{server_id}.herokuapp.com", query: { url: url } , header: { 'User-Agent'=>'a', 'Accept-Encoding'=>'a' }
   end
 
   # Returns a nokogiri document or an error if fetch response status is not 200
